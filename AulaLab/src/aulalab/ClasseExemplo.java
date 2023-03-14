5335c8d66edda2d1a32f4b14d4465671f057a2c2
Vinícius Augusto Silva Brasileiro <v247391@dac.unicamp.br>
2023-03-14 14:58:21 -0300

    Commit inicial

---
+++ AulaLab/src/aulalab/ClasseExemplo.java
@@ -0,0 +1,9 @@
+package aulalab;
+
+public class ClasseExemplo 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ AulaLab/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author viniasbr
+ *
+ */
+module AulaLab {
+}
